f9e71a82c0bb808f2b508bcf78448c85ea7bc770
CiaraMaria Proia <proiacm@gmail.com>
2020-03-25 22:20:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,8 @@ katz_deli = []
 def line(place)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  else katz_deli.length > 0  
+    katz_deli.each.with_index(1).map { |
   end    
   "The line is currently empty."
 end
