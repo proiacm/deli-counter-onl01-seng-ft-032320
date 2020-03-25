78ab02bf50ee00513dd3a3963b9738e000d31d1b
CiaraMaria Proia <proiacm@gmail.com>
2020-03-25 22:31:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@ def line(place)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else katz_deli.length > 0  
-    other_deli.each { |line| puts "#{num} #{name}"}
+    katz_deli.each { |line| puts "#{num} #{name}"}
     
   end    
   "The line is currently empty."
 end
 
 def take_a_number(num, name)
-  katz_deli << "#{num} #{name}"
+  katz_deli << other_deli
 end  
