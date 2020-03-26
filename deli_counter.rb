e6958013b76b193620be3132571c7f2ce4a66747
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 00:06:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,9 @@ puts serving
     puts "The line is currently empty."    
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
